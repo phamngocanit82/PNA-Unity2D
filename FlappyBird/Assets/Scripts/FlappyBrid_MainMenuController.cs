1b083a09d61603d478ef46f0347f6df8ed4993c4
ngocan.pham@ogilvy.com <ngocan.pham@ogilvy.com>
2018-04-20 14:37:29 +0700

    FlappyBird

---
+++ FlappyBird/Assets/Scripts/FlappyBrid_MainMenuController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+//using UnityEngine.SceneManagement;
+
+public class FlappyBrid_MainMenuController : MonoBehaviour {
+
+	public void _PlayButton(){
+		Application.LoadLevel ("FlappyBrid_GamePlay");
+	}
+}
---
+++ FlappyBird/Assets/Scripts/FlappyBrid_PipeHolder.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlappyBrid_PipeHolder : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (FlappyBrid_BirdController.instance != null) {
+			if (FlappyBrid_BirdController.instance.flag == 1) {
+				Destroy (GetComponent<FlappyBrid_PipeHolder> ());
+			}
+		}
+		_PipeMovement ();
+	}
+
+	void _PipeMovement(){
+		Vector3 temp = transform.position;
+		temp.x -= speed * Time.deltaTime;
+		transform.position = temp;
+	}
+
+	void OnTriggerEnter2D(Collider2D target){
+		if (target.tag == "Destroy") {
+			Destroy (gameObject);
+		}
+	}
+}
